6fe09566b7bb427b6325bf6c2b3cec3760011bb6
Tobias Moretti <78236685+TobiasMoretti123@users.noreply.github.com>
2024-04-21 15:11:23 -0300

    Ejercicio 4

--- Ejercicios/Ejercicio4/src/app/app.component.ts
+++ Ejercicios/Ejercicio4/src/app/app.component.ts
@@ -1,13 +1,64 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule, JsonPipe } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router, RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { Usuario } from './Componentes/Usuario';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,CommonModule,FormsModule,RouterLink,RouterLinkActive],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Ejericio4';
+  nombre = '';
+  clave = '';
+  botonApretado = false;
+  usuarios = [{
+    nombre:'Tobias',
+    clave:'12345'
+  },
+  {
+    nombre:'Army',
+    clave:'67891'
+  },
+  {
+    nombre:'Wenxi',
+    clave:'2468'
+  }];
+  constructor(private route:Router){}
+  Ingresar(nombre:string,clave:string){
+
+    this.Guardar();
+    this.botonApretado = true;
+
+    for (let i = 0; i < this.usuarios.length; i++) {
+        if(nombre === this.usuarios[i].nombre && clave === this.usuarios[i].clave){
+          this.route.navigate(['bienvenido']);
+          break; 
+        } 
+        else
+        { 
+          this.route.navigate(['error']); 
+        } 
+    }
+  }
+
+  ngOnInit(): void {
+    const usuariosStringJSON = localStorage.getItem('usuariosString');
+    if(usuariosStringJSON){
+        const usuariosRecuperados: Usuario[] = JSON.parse(usuariosStringJSON);
+        console.log(usuariosRecuperados);
+      }else{
+        console.log('No se encontraron usuarios');
+      }
+  }
+
+  Guardar(){
+    const usuariosString = JSON.stringify(this.usuarios);
+    localStorage.setItem('usuariosString',usuariosString);
+  }
+  
 }
--- Ejercicios/Ejercicio4/src/app/app.routes.ts
+++ Ejercicios/Ejercicio4/src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './Componentes/login/login.component';
+import { BienvenidoComponent } from './Componentes/bienvenido/bienvenido.component';
+import { ErrorComponent } from './Componentes/error/error.component';
 
-export const routes: Routes = [];
+
+
+export const routes: Routes = [
+    { path: '', redirectTo: '/login', pathMatch: "full" },
+    { path: 'login', component: LoginComponent },
+    { path: 'bienvenido', component: BienvenidoComponent },
+    { path: 'error', component: ErrorComponent},
+    { path: '**', component: ErrorComponent },
+];
+
