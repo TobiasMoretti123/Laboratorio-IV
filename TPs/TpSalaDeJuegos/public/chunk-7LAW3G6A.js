102a7abc55ac402e5cd20e490fe0deb129e7987e
Tobias Moretti <78236685+TobiasMoretti123@users.noreply.github.com>
2024-05-15 00:24:46 -0300

    tercer sprint

---
+++ TPs/TpSalaDeJuegos/public/chunk-7LAW3G6A.js
@@ -0,0 +1,1 @@
+import{a}from"./chunk-ONEFJIXZ.js";import"./chunk-BEWHN6CC.js";import"./chunk-QRO6ZU5W.js";import"./chunk-H5ZOCSIB.js";import"./chunk-GLVLIRRK.js";import"./chunk-XHZBIIPB.js";import"./chunk-WXI33M2S.js";import"./chunk-AKS2IQM2.js";import"./chunk-VA7YLXIX.js";import"./chunk-YPGK2BPN.js";export{a as LoginVerificacionComponent};
--- TPs/TpSalaDeJuegos/src/app/Componentes/chat/chat.component.ts
+++ TPs/TpSalaDeJuegos/src/app/Componentes/chat/chat.component.ts
@@ -3,21 +3,26 @@ import { ChatService } from '../../Servicios/chat.service';
 import { CommonModule,DatePipe } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Auth } from '@angular/fire/auth';
+import { Router } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-chat',
   standalone: true,
-  imports: [CommonModule,DatePipe,FormsModule],
+  imports: [CommonModule,DatePipe,FormsModule,MatButtonModule],
   templateUrl: './chat.component.html',
   styleUrl: './chat.component.css'
 })
 export class ChatComponent implements OnInit{
+  mensajes$: Observable<{ usuario: string, mensaje: string, fecha: Date }[]> | null = null;
   nuevoMensaje: string = '';
 
-  constructor(public chatService: ChatService,public auth:Auth) {}
+  constructor(public chatService: ChatService,public auth:Auth,public router:Router) {}
 
   ngOnInit(): void {
     this.chatService.initChat();
+    this.mensajes$ = this.chatService.mensajes$;
   }
 
 
@@ -26,5 +31,13 @@ export class ChatComponent implements OnInit{
       this.chatService.enviarMensaje(this.auth.currentUser?.email,this.nuevoMensaje);
       this.nuevoMensaje = ''; 
     }
+  }
+
+  Volver(){
+    this.RuteoHome();
+  }
+  
+  RuteoHome(){
+    this.router.navigate(['/home']);
   }
 }
