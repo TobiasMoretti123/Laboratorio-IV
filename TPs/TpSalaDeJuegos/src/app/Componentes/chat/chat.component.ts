5af1e0ac3b714e5f6f344ad19e67b0cf0fdd5b8c
Tobias Moretti <78236685+TobiasMoretti123@users.noreply.github.com>
2024-05-14 23:19:36 -0300

    tercer sprint

--- TPs/TpSalaDeJuegos/src/app/Componentes/chat/chat.component.ts
+++ TPs/TpSalaDeJuegos/src/app/Componentes/chat/chat.component.ts
@@ -1,16 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { ChatService } from '../../Servicios/chat.service';
+import { CommonModule,DatePipe } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Auth } from '@angular/fire/auth';
 
 @Component({
   selector: 'app-chat',
+  standalone: true,
+  imports: [CommonModule,DatePipe,FormsModule],
   templateUrl: './chat.component.html',
   styleUrl: './chat.component.css'
 })
-export class ChatComponent implements OnInit {
+export class ChatComponent implements OnInit{
+  nuevoMensaje: string = '';
 
-  constructor() { }
+  constructor(public chatService: ChatService,public auth:Auth) {}
 
-  ngOnInit() {
-    
+  ngOnInit(): void {
+    this.chatService.initChat();
   }
-  
+
+
+  enviarMensaje() {
+    if (this.nuevoMensaje.trim() !== '') {
+      this.chatService.enviarMensaje(this.auth.currentUser?.email,this.nuevoMensaje);
+      this.nuevoMensaje = ''; 
+    }
+  }
 }
--- TPs/TpSalaDeJuegos/src/app/Servicios/chat.service.ts
+++ TPs/TpSalaDeJuegos/src/app/Servicios/chat.service.ts
@@ -1,8 +1,38 @@
 import { Injectable } from '@angular/core';
+import { Firestore, collection, collectionData, DocumentData } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { addDoc } from '@angular/fire/firestore';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ChatService {
+  mensajes$: Observable<{ usuario: string, mensaje: string, fecha: Date }[]> | null = null;
+  nuevoMensaje: string = '';
+
+  constructor(private firestore: Firestore) {
+    
+  }
+
+  initChat() {
+    let col = collection(this.firestore, 'messages');
+    const observable = collectionData(col).pipe(
+      map((data: DocumentData[]) => {
+        return data.map(doc => {
+          return {
+            usuario: doc['usuario'],
+            mensaje: doc['mensaje'],
+            fecha: doc['fecha'].toDate()
+          };
+        });
+      })
+    );
+    this.mensajes$ = observable;
+  }
   
+  enviarMensaje(usuario: string | null | undefined, mensaje: string) {
+    let col = collection(this.firestore, 'messages');
+    addDoc(col, { fecha: new Date(), 'usuario': usuario, 'mensaje': mensaje });
+  }
 }
